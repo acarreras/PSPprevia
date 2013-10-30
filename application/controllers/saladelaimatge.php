ca74782ee2edf2b2f6e206820f171b20b3f0b92e
acarreras <carreras.anna@gmail.com>
2013-10-31 00:13:18 +0100

    sala de la imatge (tot el que es pot de moment)

--- application/controllers/saladelaimatge.php
+++ application/controllers/saladelaimatge.php
@@ -2,16 +2,38 @@
 	class Saladelaimatge extends CI_Controller {
 	function __construct(){
 		parent::__construct();
+		$this->load->model('sala','',TRUE);
+		$this->load->model('apartats','',TRUE);
+		$this->load->model('respostes','',TRUE);
+		$this->load->helper(array('form', 'url'));
 	}
 
 	function index(){
 		$this->load->library(array('form_validation'));
-		$this->load->helper(array('form', 'url'));
 
 		if($this->session->userdata('logged_in'))
 		{
 			$session_data = $this->session->userdata('logged_in');
 			$data['username'] = $session_data['username'];
+			$result = $this->sala->getSalaById(4);
+			foreach($result as $row)
+			{
+				$data['titol'] = $row->titol;
+				$data['salanext'] = $row->salanext;
+				$data['salaprev'] = $row->salaprev;
+			}
+			$data['titolapartat1'] = $this->apartats->getApartatTitol(8);
+			$data['titolapartat2'] = $this->apartats->getApartatTitol(9);
+			$data['titolapartat3'] = $this->apartats->getApartatTitol(10);
+			
+			// TODO: no sé comptar amb querys :-(
+			$data['bapartat1fet'] = false;//$this->respostes->bapartatJaFet($session_data['username'], 4,1);
+			$data['bapartat2fet'] = false;//$this->respostes->bapartatJaFet($session_data['username'], 4,2);
+			$data['bapartat3fet'] = true;//$this->respostes->bapartatJaFet($session_data['username'], 4,3);
+			
+			$data['perquegraffitipropi'] = $this->respostes->getLaMevaRespostaText($session_data['username'], 4,2);
+			$data['perquegraffitialtres'] = $this->respostes->getAltresRespostaText($session_data['username'], 4,2);
+			
 			$this->load->view('saladelaimatge_view', $data);
 		}
 		else
@@ -19,6 +41,23 @@
 			//If no session, redirect to login page
 			redirect('login', 'refresh');
 		}
+		
 	}
+	
+	function guardarGraffiti(){
+		$session_data = $this->session->userdata('logged_in');
+		
+		$results = $this->respostes->getUltimsTexts(4,2);
+		$str = '';
+		foreach ($results as  $row) {
+			$str .= $row->respostatext;
+			$str .= "<br/>";
+		}
+		
+		$this->respostes->guardarText($this->input->post('titol'), $session_data['username'], 4,2);
+		
+		echo $str;
+	}
+	
 }
 ?>
--- application/views/saladelaimatge_view.php
+++ application/views/saladelaimatge_view.php
@@ -1,7 +1,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>PSP sala de la imatge</title>
+	<title><?php echo($titol);?></title>
 	<style>
 		html { 
 			background: url(<?php echo base_url().'assets/images/fons/salaimatge-fonsnet.png'; ?>) no-repeat center center fixed; 
@@ -12,18 +12,66 @@
 		}
 	</style>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/styles/site.css'; ?>">
+	<!--Load JQUERY from Google's network -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	<script> 
+    // using JQUERY's 
+    $(document).ready(function () {
+		$("#saladelaimatgegraffitiok").click(function () {
+			$.post("<?php echo base_url()?>index.php/saladelaimatge/guardarGraffiti", {titol : $("#saladelaimatgegraffitiperque").val()})
+				.done(function(resultat) {
+					// TODO: retornar per separat els texts (parsejar?) i les imatges (parsejar imatges eing?)
+					$("#resultatgraffitis").html(resultat);
+				});
+		});
+	});
+  </script>
 </head>
 <body>
-	<!-- menú superior dreta-->
-	<div class="capcaleratitol">Sala de la IMATGE</div>
+<div id="res"><div>
+	<!-- menú superior dreta -->
+	<div class="capcaleratitol"><?php echo($titol);?></div>
 	<a class="capcalerasortir right" href="home/logout">SORTIR</a>
 	<div class="capcalerabarra right">I</div>
 	<div class="capcalerausername right"><?php echo($username); ?></div>
 	<br/><br/>
 	<a class="capcalerahome right" href="home">^</a>
 	<div class="capcalerabarra right">I</div>
-	<a class="capcalerafletxa right" href="saladeljo">></a>
-	<div class="capcalerabarra right">I</div>
-	<a class="capcalerafletxa right" href="saladelso"><</a>
+	<?php if($salanext != NULL){ ?>
+		<a class="capcalerafletxa right" href="<?php echo($salanext); ?>">></a>
+		<div class="capcalerabarra right">I</div>
+	<?php } ?>
+	<?php if($salaprev != NULL){ ?>
+		<a class="capcalerafletxa right" href="<?php echo($salaprev); ?>"><</a>
+		<div class="capcalerabarra right">I</div>
+	<?php } ?>
+	
+	<!-- continguts -->
+	<div class="contingutsbox">
+		<!-- filtra -->
+		<div class="contingutstitol"><?php echo($titolapartat1); ?></div>
+		<div class="hr"><hr/></div>
+		<img class="contingutsimatge50percent" src="<?php echo base_url().'/assets/images/saladelaimatge/filtra01.png'; ?>" />
+		
+		<!-- puja imatge carrer -->
+		<div class="contingutstitol"><?php echo($titolapartat2); ?></div>
+		<div class="hr"><hr/></div>
+		<?php if ($bapartat2fet == false) {?>
+			<form>
+				<input type="text" class="contingutstextformrequadre" id="saladelaimatgegraffitiperque" name="saladelaimatgegraffitiperque"/>
+				<input id="saladelaimatgegraffitiok" type="button" value="ok"/>
+				<div class="contingutstexttitolresposta">Què han trobat pel carrer els altres artístes?</div>
+				<div class="contingutsboxresposta" id="resultatgraffitis"></div>
+			</form>
+		<?php } else { ?>
+			<div class="contingutstexttitolresposta">Què vas escriure tu?</div>
+			<div class="contingutsboxresposta"><?php echo($perquegraffitipropi); ?></div>
+			<div class="contingutstexttitolresposta">Què van escriure els altres participants?</div>
+			<div class="contingutsboxresposta"><?php echo($perquegraffitialtres); ?></div>
+		<?php } ?>
+		<!-- fotograma -->
+		<div class="contingutstitol"><?php echo($titolapartat3); ?></div>
+		<div class="hr"><hr/></div>
+	</div>
 </body>
 </html>
