09a1869c608de1fe2bb9d5377edcc2206f48d360
acarreras <carreras.anna@gmail.com>
2013-11-07 02:59:52 +0100

    test del background de home amb pinzellades

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,34 +1,45 @@
-
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 session_start(); //we need to call PHP's session object to access it through CI
+
 class Home extends CI_Controller {
 
- function __construct()
- {
-   parent::__construct();
- }
+	function __construct() {
+		parent::__construct();
+		$this->load->model('respostes','',TRUE);
+	}
 
- function index()
- {
-   if($this->session->userdata('logged_in'))
-   {
-     $session_data = $this->session->userdata('logged_in');
-     $data['username'] = $session_data['username'];
-     $this->load->view('home_view', $data);
-   }
-   else
-   {
-     //If no session, redirect to login page
-     redirect('login', 'refresh');
-   }
- }
+	function index(){
+		if($this->session->userdata('logged_in')){
+			$session_data = $this->session->userdata('logged_in');
+			$data['username'] = $session_data['username'];
+			
+			// sala 1
+			$bapartat11fet = $this->respostes->bapartatJaFet($session_data['username'], 1,1);
+			$bapartat12fet = $this->respostes->bapartatJaFet($session_data['username'], 1,2);
+			$bapartat131fet = $this->respostes->bapartatJaFet($session_data['username'], 1,31);
+			$bapartat132fet = $this->respostes->bapartatJaFet($session_data['username'], 1,32);
+			$bapartat133fet = $this->respostes->bapartatJaFet($session_data['username'], 1,33);
+			
+			if($bapartat11fet == true && $bapartat12fet == true && $bapartat131fet == true && $bapartat132fet == true && $bapartat133fet == true){
+				$data['bsala1feta'] = true;
+			}
+			else{
+				$data['bsala1feta'] = false;
+			}
+			
+			$this->load->view('home_view', $data);
+		}
+		else{
+			//If no session, redirect to login page
+			redirect('login', 'refresh');
+		}
+	}
 
- function logout()
- {
-   $this->session->unset_userdata('logged_in');
-   session_destroy();
-   redirect('home', 'refresh');
- }
+	function logout(){
+		$this->session->unset_userdata('logged_in');
+		session_destroy();
+		redirect('home', 'refresh');
+	}
 
 }
 
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -4,7 +4,10 @@
 	<title>PSP galeria</title>
 	<style>
 		html { 
-			background: url(<?php echo base_url().'assets/images/fons/mapa-fonsnet.png'; ?>) no-repeat center center fixed; 
+			background: url(<?php echo base_url().'assets/images/fons/mapa-fonsnet.png'; ?>);
+			background-repeat: no-repeat;
+			background-position: center center;
+			background-attachment: fixed;
 			-webkit-background-size: cover;
 			-moz-background-size: cover;
 			-o-background-size: cover;
@@ -12,17 +15,81 @@
 		}
 	</style>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/styles/site.css'; ?>">
+	<!--Load JQUERY from Google's network -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	<script> 
+    // using JQUERY's 
+    $(document).ready(function () {
+		$num = 3;
+		fons1();
+		var function1 = function fons1() {
+			if($num > 0) {
+				alert('inside 1');
+				$stringurl = "url(<?php echo base_url().'assets/images/pinzellades/salaLLENGUATGEover.png'; ?>), " + $('html').css('background');
+				$('html').css('background', $stringurl);
+				$stringrepeat = "no-repeat, " + $('html').css('background-repeat');
+				$('html').css('background-repeat', $stringrepeat);
+				$stringposition = "center center, " + $('html').css('background-position');
+				$('html').css('background-position', $stringposition);
+				$stringattachment = "fixed, " + $('html').css('background-attachment');
+				$stringbckgrndsize = "cover, cover";
+				$('html').css('background-size', $stringbckgrndsize);
+				$('html').css('background-attachment', $stringattachment);
+				$stringwebkit = "cover, " + $('html').css('-webkit-background-size');
+				$('html').css('-webkit-background-size', $stringwebkit);
+				$stringmoz = "cover, " + $('html').css('-moz-background-size');
+				$('html').css('-moz-background-size', $stringmoz);
+				$stringo = "cover, " + $('html').css('-o-background-size');
+				$('html').css('-o-background-size', $stringo);
+			}
+			return $.ajax(...);
+		}
+		function1().done(fons2);
+
+		var function2 = function fons2() {
+			alert('inside 2');
+			if($num > 1) {
+				$stringurl = "url(<?php echo base_url().'assets/images/pinzellades/salaSOover.png'; ?>), " + $('html').css('background');
+				$('html').css('background', $stringurl);
+				$stringrepeat = "no-repeat, " + $('html').css('background-repeat');
+				$('html').css('background-repeat', $stringrepeat);
+				$stringposition = "center center, " + $('html').css('background-position');
+				$('html').css('background-position', $stringposition);
+				$stringattachment = "fixed, " + $('html').css('background-attachment');
+				$stringbckgrndsize = "cover, cover";
+				$('html').css('background-size', $stringbckgrndsize);
+				$('html').css('background-attachment', $stringattachment);
+				$stringwebkit = "cover, " + $('html').css('-webkit-background-size');
+				$('html').css('-webkit-background-size', $stringwebkit);
+				$stringmoz = "cover, " + $('html').css('-moz-background-size');
+				$('html').css('-moz-background-size', $stringmoz);
+				$stringo = "cover, " + $('html').css('-o-background-size');
+				$('html').css('-o-background-size', $stringo);
+			}
+			return $.ajax(...);
+		}
+	});
+  </script>
 </head>
 <body>
-<a href="<?php echo ('saladelesparaules'); ?>">1</a>
-<a href="<?php echo ('saladelso'); ?>">2</a>
-<a href="<?php echo ('saladelaimatge'); ?>">3</a>
-<a href="<?php echo ('saladeljo'); ?>">4</a>
-<a href="<?php echo ('biblioteca'); ?>">5</a>
-<a href="<?php echo ('exposicioglobal'); ?>">6</a>
-<a href="<?php echo ('saladeldolorilafelicitat'); ?>">7</a>
-<a href="<?php echo ('wc'); ?>">8</a>
-<a href="<?php echo ('lamevaexposicio'); ?>">logout</a>
-</body>
+	<!-- menú superior dreta -->
+	<a class="capcalerasortir right" href="home/logout">SORTIR</a>
+	<div class="capcalerabarra right">I</div>
+	<div class="capcalerausername right"><?php echo($username); ?></div>
+	
+	<?php if ($bsala1feta == true) { ?>
+	
+	<?php } else { ?>
+	<?php } ?>
+		
+	<a href="<?php echo ('saladelesparaules'); ?>">1</a>
+	<a href="<?php echo ('saladelso'); ?>">2</a>
+	<a href="<?php echo ('saladelaimatge'); ?>">3</a>
+	<a href="<?php echo ('saladeljo'); ?>">4</a>
+	<a href="<?php echo ('biblioteca'); ?>">5</a>
+	<a href="<?php echo ('exposicioglobal'); ?>">6</a>
+	<a href="<?php echo ('saladeldolorilafelicitat'); ?>">7</a>
+	<a href="<?php echo ('wc'); ?>">8</a>
+	</body>
 </html>
 
